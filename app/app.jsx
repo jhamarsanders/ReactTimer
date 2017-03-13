e9ca7d2211f8c694f15aa34c2a1671062ca22bb8
Jay Sanders <j.sanders@realitychecksystems.com>
2017-03-13 16:12:25 -0700

    Timer and Countdown Component

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
+var Timer = require('Timer');
+var Countdown = require('Countdown');
 
 require('style-loader!css-loader!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
@@ -11,6 +13,8 @@ require('style-loader!css-loader!sass-loader!applicationStyles');
 ReactDOM.render(
     <Router history={hashHistory}>
         <Route path="/" component={Main}>
+            <Route path="countdown" component={Countdown}/>
+            <IndexRoute component={Timer}/>
         </Route>
     </Router>,
     document.getElementById('app')
