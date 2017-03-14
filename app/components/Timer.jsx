6b4dfe157f880d4964605e66a8de8fcf39c9330b
Jay Sanders <j.sanders@realitychecksystems.com>
2017-03-14 15:51:21 -0700

    Timer Working

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,8 +1,50 @@
 var React = require('react');
+var Clock = require('Clock');
+var TimerControls = require('TimerControls');
 
 var Timer = React.createClass({
+    getInitialState: function(){
+        return {
+            count: 0,
+            timerStatus: 'stopped'
+        }
+    },
+    componentDidUpdate: function(prevProps, prevState){
+        if(this.state.timerStatus !== prevState.timerStatus){
+            switch(this.state.timerStatus){
+                case 'started':
+                    this.startTimer();
+                    break;
+                case 'cleared':
+                    this.setState({count: 0});
+                case 'stopped':
+                    clearInterval(this.timer);
+                    this.timer = undefined;
+                    break;
+            }
+        }
+    },
+    startTimer: function() {
+        this.timer = setInterval(() => {
+            var newCount = this.state.count + 1;
+            this.setState({
+                count: newCount >= 0 ? newCount: 0
+            });
+        }, 1000);
+    },
+    handleStatusChange: function(newStatus){
+        this.setState({timerStatus: newStatus});
+    },
     render: function(){
-        return <p>Timer.jsx</p>
+        var {count, timerStatus} = this.state;
+
+        return (
+            <div>
+                <h1 className="page-title">Timer</h1>
+                <Clock totalSeconds={count}/>
+                <TimerControls timerStatus={timerStatus} onStatusChange={this.handleStatusChange}/>
+            </div>
+        )
     }
 });
 
